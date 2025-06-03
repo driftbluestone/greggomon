30e3cd28059755568a89c9328b66fb4908f0e509
driftbluestone <117239036+driftbluestone@users.noreply.github.com>
2025-06-02 19:06:05 -0700

    v1.1.0
    - Update replaces.json to include substation hatches, 'hex':'16X' is now 'hex_':'16X ' to prevent getting mixed up with any images with hex in their name.\n- upper.json how requires spaces after every voltage tier to prevent conflits.\n- Fix scoreboard. No longer generates ghost entries with usernames instead of user id's when using the /score command. Numbering is now correct, for example page 2 now starts at 11 instead of 10.\n- Added /skip command, requires 3 people that have used all 3 guesses to skip after 10 minutes have passed since the last image.\n- add info command to access changelogs and help.\n- add info subcommands: `1.0.1`, `1.1.0`, `help`.\n- add reduceproblist.txt, so when an image with any keywords in the list are found, it re-rolls.\n- quit is now a slash command.\n- /image command description now specifies that it is admin-only.
    (this is just copied from the changelog viewable on discord).

--- wtg.py
+++ wtg.py
@@ -1,14 +1,8 @@
 # CONFIG
-TOKEN = ""
 DIR ="C:/wtg-testing"
-
+TOKEN =""
 import discord # type: ignore
-import random
-import asyncio
-import shutil
-import json
-import typing
-import math
+import random, asyncio, shutil, json, typing, math, time
 from discord.ext import commands # type: ignore 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
@@ -19,16 +13,21 @@ ALLOWEDUSERS = []
 ALLOWEDROLES = []
 QUITLIST = []
 IMAGESET = []
+REDUCEPROBLIST = []
+SUBCOMMANDS ={}
 user_attempts = {}
+TIME = 0
 
 def load_data():
-    global REPLACES, UPPER, QUITLIST, IMAGESET, USER_SCORE, ALLOWEDUSERS, ALLOWEDROLES
+    global REPLACES, UPPER, QUITLIST, IMAGESET, USER_SCORE, ALLOWEDUSERS, ALLOWEDROLES, REDUCEPROBLIST, SUBCOMMANDS
     with open(f"{DIR}/replaces.json", "r") as file:
         REPLACES = json.load(file)
     with open(f"{DIR}/upper.json", "r") as file:
         UPPER = json.load(file)
     with open(f"{DIR}/quitlist.txt", "r") as file:
         QUITLIST = [line.strip() for line in file]
+    with open(f"{DIR}/reduceproblist.txt", "r") as file:
+        REDUCEPROBLIST = [line.strip() for line in file]
     with open(f"{DIR}/imagelist.txt", "r") as file:
         IMAGESET = [line.strip() for line in file]
     with open(f"{DIR}/userscore.json", "r") as file:
@@ -37,6 +36,8 @@ def load_data():
         ALLOWEDUSERS = [line.strip() for line in file]
     with open(f"{DIR}/allowedroles.txt", "r") as file:
         ALLOWEDROLES = [line.strip() for line in file]
+    with open(f"{DIR}/subcommands.json", "r") as file:
+        SUBCOMMANDS = json.load(file)
 load_data()
 
 @bot.event
@@ -60,12 +61,20 @@ def uppervolt(text):
     return text
 
 async def sendImage(target, msgCont):
-    global image, item, itemAnswerList, totalGuesses, user_attempts
+    global image, item, itemAnswerList, totalGuesses, user_attempts, TIME, allAnswerFoundList
     totalGuesses = 0
 
     user_attempts = {}
+    allAnswerFoundList=[]
 
     item = random.choice(IMAGESET)
+    for k in REDUCEPROBLIST:
+        if k in item:
+            print(item)
+            print("Image re-rolled.")
+            item = random.choice(IMAGESET)
+    TIME = int(time.time())
+    print(TIME)
     image = f"{DIR}/imagelist/{item}"
     print(image)
     rimage = shutil.copyfile(f"{DIR}/imagelist/{item}",f"{DIR}/item.png")
@@ -81,18 +90,61 @@ async def sendImage(target, msgCont):
     else:
         await target.response.send_message(msgCont, file=file)
 
-@bot.command("quit")
-async def quit_bot(ctx):
-    print(ctx.author.roles)
-    print(ctx.author.id)
-    if (str(ctx.author.id) in ALLOWEDUSERS) or (discord.utils.find(lambda r: str(r.id) in ALLOWEDROLES, ctx.author.roles)):
-        await ctx.reply("o7")
+@bot.tree.command(name="skip", description="Skips the current image if it's been 10 minutes since it was sent. Requires 3 people to skip.")
+async def skip(interaction: discord.Interaction):
+    global TIME, user_attempts, item
+    processed=""
+    try:
+        if skiplist: pass
+    except: skiplist = []
+    if TIME+1800 <= int(time.time()):
+        await interaction.response.send_message(f"The answer was:\n{uppervolt(item.title())}")
+        await sendImage(interaction, "")
+    elif TIME+600 <= int(time.time()):
+        if interaction.user.id not in skiplist:
+            try:
+                if not user_attempts[str(interaction.user.id)] == 3:
+                    await interaction.response.send_message("You have to have used all 3 guesses to /skip.",ephemeral=True)
+                else:
+                    skiplist.append(interaction.user.id)
+                    processed=f"Skip processed. {len(skiplist)}/3.\n"
+                    if len(skiplist) == 3:
+                        await interaction.response.send_message(f"{processed}The answer was:\n{uppervolt(item.title())}")
+                        skiplist = []
+                        await sendImage(interaction, "")
+                    else:
+                        await interaction.response.send_message(f"{processed}\n")
+            except:
+                await interaction.response.send_message("You have to have used all 3 guesses to /skip.",ephemeral=True)
+        else:
+            await interaction.response.send_message("You've already used /skip for this image! Try again after 30 minutes since the image was sent.", ephemeral=True)
+    else:
+        await interaction.response.send_message("It hasn't been 10 minutes yet!", ephemeral=True)
+
+@bot.tree.command(name="info", description="Help and changelogs.")
+async def info(interaction: discord.Interaction, sub: typing.Optional[str]):
+    if sub==None:
+        embed=discord.Embed(title="Available Subcommands", description=str(list(SUBCOMMANDS.keys())))
+    else:
+        try:
+            embed=discord.Embed(title=sub.title(), description=SUBCOMMANDS[sub])
+        except:
+            await interaction.response.send_message("", ephemeral=True)
+            return
+    await interaction.response.send_message(embed=embed, ephemeral=True)
+
+@bot.tree.command(name="quit", description="**Admin Only.** Kills the bot.")
+async def quit_bot(ctx: discord.Interaction):
+    print(ctx.user.roles)
+    print(ctx.user.id)
+    if (str(ctx.user.id) in ALLOWEDUSERS) or (discord.utils.find(lambda r: str(r.id) in ALLOWEDROLES, ctx.user.roles)):
+        await ctx.response.send_message("o7")
         await quit()
     else:
-        await ctx.reply(random.choice(QUITLIST))
+        await ctx.response.send_message(random.choice(QUITLIST))
 
 async def processMessage(source, content_lower, user_id):
-    global totalGuesses, answerFoundList, user_attempts
+    global totalGuesses, answerFoundList, user_attempts, allAnswerFoundList
     content_lower = cleanInput(content_lower)
     response_method = source.followup.send
     responseList = content_lower.split()
@@ -119,35 +171,31 @@ async def processMessage(source, content_lower, user_id):
     else:
         if user_id not in user_attempts:
             user_attempts[user_id] = 0
-
-        if not answerFoundList:
-            user_attempts[user_id] += 1
-            if totalGuesses >= 10:
-                await hint_message_slash(source)
+        hint=""
+        user_attempts[user_id] += 1
+        guessLeft=""
+        if user_attempts[user_id] == 2:
+            guessLeft="You have one guess left.\n"
+        if user_attempts[user_id] == 3:
+            guessLeft="You've run out of guesses.\n"
+        for i in answerFoundList:
+            allAnswerFoundList.append(i)
+        if totalGuesses == 10:
+            possible_hints = [x for x in itemAnswerList if x not in allAnswerFoundList]
+            print(allAnswerFoundList)
+            print(possible_hints)
+            if not possible_hints:
+                hint = "\n10 incorrect Guesses, huh? Heres a hint.\nAll of the words in the item name have been found already."
             else:
-                await response_method("Nope!")
+                hint = f"\n10 incorrect Guesses, huh? Heres a hint.\nOne of the words in the item name is: {random.choice(possible_hints)}"
+        if not answerFoundList:
+            await response_method(f"Nope! {guessLeft}{hint}")
         else:
-            user_attempts[user_id] += 1
-            GuessLeftStatement = ""
-            match(user_attempts[user_id]):
-                case 2:
-                    GuessLeftStatement = " You have one guess left."
-                case 3:
-                    GuessLeftStatement = " You're out of guesses."
-                case _:
-                    GuessLeftStatent = ""
-            await response_method(f"Not Quite!{GuessLeftStatement} Correct words: {answerFoundList}")
+            await response_method(f"Not Quite! {guessLeft}Correct words: {answerFoundList}{hint}")
 
     await asyncio.to_thread(print, user_attempts)
 
-async def hint_message_slash(interaction: discord.Interaction):
-    possible_hints = [x for x in itemAnswerList if x not in answerFoundList]
-    if not possible_hints:
-        await interaction.followup.send("10 incorrect Guesses, huh? Heres a hint.\nAll of the words in the item name have been found already.", ephemeral=False)
-        return
-    await interaction.followup.send(f"10 incorrect Guesses, huh? Heres a hint.\nOne of the words in the item name is: {random.choice(possible_hints)}")
-
-@bot.tree.command(name="image", description="Sends a new image.")
+@bot.tree.command(name="image", description="**Admin Only.** Sends a new image.")
 async def image(interaction: discord.Interaction):
     if (str(interaction.user.id) in ALLOWEDUSERS) or (discord.utils.find(lambda r: str(r.id) in ALLOWEDROLES, interaction.user.roles)):
         await interaction.response.defer()
@@ -171,16 +219,19 @@ async def answer_r(interaction, user_input):
     content_lower = user_input.lower()
 
     if user_attempts.get(user_id, 0) < 3:
-        await processMessage(interaction, content_lower, user_id)
+        if len(content_lower) > 75:
+            await interaction.followup.send("Guess is longer than max length of 75 characters.")
+        else:
+            await processMessage(interaction, content_lower, user_id)
     else:
         await interaction.followup.send("You've used all your guesses!")
 
-@bot.tree.command(name="reset", description="**Admin Only.** Reset guesses and hints for user.")
+@bot.tree.command(name="reset", description="**Admin Only.** Reset guesses for user.")
 async def resetGuesses(ctx: discord.Interaction):
     global user_attempts
     if (str(ctx.user.id) in ALLOWEDUSERS) or (discord.utils.find(lambda r: str(r.id) in ALLOWEDROLES, ctx.user.roles)):
         user_attempts.pop(str(ctx.user.id), None)
-        await ctx.response.send_message(f"{ctx.user.mention}, Guesses and hits reset.", ephemeral=True)
+        await ctx.response.send_message(f"{ctx.user.mention}, Guesses reset.", ephemeral=True)
     else:
         await ctx.response.send_message("Permission denied.", ephemeral=True)
         return
@@ -212,9 +263,11 @@ async def scorepg(interaction: discord.Interaction, page: typing.Optional[int]):
     if page > int(math.ceil((len(USER_SCORE)/10))) or page < 1:
         await interaction.response.send_message(f"Invalid page! Must be between 1 and {int(math.ceil((len(USER_SCORE)/10)))}.")
     else:
-        user_id = str(interaction.user)
+        user_id = str(interaction.user.id)
         if user_id not in USER_SCORE:
                USER_SCORE[user_id] = 0
+        with open(f"{DIR}/userscore.json", "w") as file:
+            json.dump(USER_SCORE, file)
         klist = []
         vlist = []
         sorted_user_score = sorted(USER_SCORE.items(), key=lambda x: x[1], reverse=True)
@@ -226,7 +279,7 @@ async def scorepg(interaction: discord.Interaction, page: typing.Optional[int]):
         for i in range(10):
             pos2=i+(10*(page-1))
             try:
-                descbuilder=descbuilder+(f"{str(pos2)}. <@{int(klist[pos2])}>: {vlist[pos2]}\n")
+                descbuilder=descbuilder+(f"{str(pos2+1)}. <@{int(klist[pos2])}>: {vlist[pos2]}\n")
             except: pass
 
         embed = discord.Embed(title=f"Page {page} of {int(math.ceil((len(USER_SCORE)/10)))}", description=descbuilder)
@@ -241,6 +294,8 @@ async def userscore(interaction: discord.Interaction, user: typing.Optional[disc
     user_id = str(user.id)
     if user_id not in USER_SCORE:
            USER_SCORE[user_id] = 0
+    with open(f"{DIR}/userscore.json", "w") as file:
+        json.dump(USER_SCORE, file)
     klist = []
     vlist = []
     sorted_user_score = sorted(USER_SCORE.items(), key=lambda x: x[1], reverse=True)
@@ -255,7 +310,7 @@ async def userscore(interaction: discord.Interaction, user: typing.Optional[disc
         if pos2<0: pass
         else:
             try:
-                descbuilder=descbuilder+(f"{str(pos2)}. <@{int(klist[pos2])}>: {vlist[pos2]}\n")
+                descbuilder=descbuilder+(f"{str(pos2+1)}. <@{int(klist[pos2])}>: {vlist[pos2]}\n")
             except: pass
         i+=1
     embed = discord.Embed(title=f"{user.name}'s score", color=0x222222, description=descbuilder)
